5abab552c27bae89f053fc696e33a83f9ac5703d
Gleb Garanin <glebgar567@gmail.com>
2021-10-31 20:47:10 +0200

    change project structure and add `dynaconf` to dependencies

--- web/api/v1/__init__.py
+++ web/api/v1/__init__.py
@@ -1,1 +1,15 @@
 from __future__ import annotations
+
+from typing import Optional
+
+from fastapi import APIRouter
+
+from web.api.v1 import order
+
+v1_router = APIRouter(prefix="api/v1")
+
+
+def install_v1_api(main_router: Optional[APIRouter] = None) -> None:
+    if main_router is not None:
+        main_router.include_router(v1_router)
+    v1_router.include_router(order.router)
