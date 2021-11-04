001e64580149ec6743386037853b9d46efe97c87
Gleb Garanin <glebgar567@gmail.com>
2021-11-04 20:58:06 +0200

    Add framework events

---
+++ web/events.py
@@ -0,0 +1,9 @@
+from blacksheep.server import Application
+
+
+async def on_startup(application: Application) -> None:
+    pass
+
+
+async def on_shutdown(application: Application) -> None:
+    pass
